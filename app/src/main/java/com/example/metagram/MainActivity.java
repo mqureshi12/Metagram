98132a70563c68b34f62dd25b4b148ebe01ca426
Mohammad Qureshi <mqure016@fiu.edu>
2022-06-13 15:38:34 -0700

    implemented logout

--- app/src/main/java/com/example/metagram/MainActivity.java
+++ app/src/main/java/com/example/metagram/MainActivity.java
@@ -2,13 +2,46 @@ package com.example.metagram;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.parse.LogOutCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String TAG = "MainActivity";
+    private Button btnLogout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnLogout = findViewById(R.id.btnLogout);
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ParseUser.logOutInBackground(new LogOutCallback() {
+                    @Override
+                    public void done(ParseException e) {
+                        if(e != null)
+                        {
+                            Log.e(TAG, "Issue with logout", e);
+                            Toast.makeText(MainActivity.this, "Issue with logout", Toast.LENGTH_SHORT).show();
+                        } else {
+                            Intent i = new Intent(MainActivity.this, LoginActivity.class);
+                            startActivity(i);
+                            Toast.makeText(MainActivity.this, "Success!", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        });
     }
 }
