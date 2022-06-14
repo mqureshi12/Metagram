77a8bec04e53f77511b943ecde96e3865264ecc6
Mohammad Qureshi <mqure016@fiu.edu>
2022-06-13 18:22:41 -0700

    can now query the parse db

--- app/src/main/java/com/example/metagram/MainActivity.java
+++ app/src/main/java/com/example/metagram/MainActivity.java
@@ -7,16 +7,26 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.parse.FindCallback;
 import com.parse.LogOutCallback;
 import com.parse.ParseException;
+import com.parse.ParseQuery;
 import com.parse.ParseUser;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     public static final String TAG = "MainActivity";
     private Button btnLogout;
+    private EditText etDescription;
+    private Button btnCaptureImage;
+    private ImageView ivPostImage;
+    private Button btnSubmit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,23 +34,51 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         btnLogout = findViewById(R.id.btnLogout);
+        etDescription = findViewById(R.id.etDescription);
+        btnCaptureImage = findViewById(R.id.btnCaptureImage);
+        ivPostImage = findViewById(R.id.ivPostImage);
+        btnSubmit = findViewById(R.id.btnSubmit);
+
+        queryPosts();
+
         btnLogout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                ParseUser.logOutInBackground(new LogOutCallback() {
-                    @Override
-                    public void done(ParseException e) {
-                        if(e != null)
-                        {
-                            Log.e(TAG, "Issue with logout", e);
-                            Toast.makeText(MainActivity.this, "Issue with logout", Toast.LENGTH_SHORT).show();
-                        } else {
-                            Intent i = new Intent(MainActivity.this, LoginActivity.class);
-                            startActivity(i);
-                            Toast.makeText(MainActivity.this, "Success!", Toast.LENGTH_SHORT).show();
-                        }
-                    }
-                });
+                logoutUser();
+            }
+        });
+    }
+
+    private void queryPosts() {
+        ParseQuery<Post> query = ParseQuery.getQuery(Post.class);
+        query.include(Post.KEY_USER);
+        query.findInBackground(new FindCallback<Post>() {
+            @Override
+            public void done(List<Post> posts, ParseException e) {
+                if(e != null) {
+                    Log.e(TAG, "Issue with getting posts", e);
+                    return;
+                }
+                for(Post post : posts) {
+                    Log.i(TAG, "Post: " + post.getDescription() + ", username: " + post.getUser().getUsername());
+                }
+            }
+        });
+    }
+
+    private void logoutUser() {
+        ParseUser.logOutInBackground(new LogOutCallback() {
+            @Override
+            public void done(ParseException e) {
+                if(e != null)
+                {
+                    Log.e(TAG, "Issue with logout", e);
+                    Toast.makeText(MainActivity.this, "Issue with logout", Toast.LENGTH_SHORT).show();
+                } else {
+                    Intent i = new Intent(MainActivity.this, LoginActivity.class);
+                    startActivity(i);
+                    Toast.makeText(MainActivity.this, "Success!", Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
--- app/src/main/java/com/example/metagram/ParseApplication.java
+++ app/src/main/java/com/example/metagram/ParseApplication.java
@@ -3,12 +3,14 @@ package com.example.metagram;
 import android.app.Application;
 
 import com.parse.Parse;
+import com.parse.ParseObject;
 
 public class ParseApplication extends Application {
 
     @Override
     public void onCreate() {
         super.onCreate();
+        ParseObject.registerSubclass(Post.class);
         Parse.initialize(new Parse.Configuration.Builder(this)
                 .applicationId("O2sJfeuAw5pSUoVFZ0AkoVHDcpYBQbGKUBtj0Byk")
                 .clientKey("MKeop6F1PNRtHrTuAn3oHx5sOcupIIAeHYtN0Oup")
